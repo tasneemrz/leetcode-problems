df2c9c3c8ce319096471889a0a1148480c6f8941
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-09-14 22:39:11 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (47.09%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n+1);
+        vector<int> ans(n+1, 0);
 
         for(int i = 0; i <= n; i++) {
-            int count_1 = 0;
-            int num = i;
-
-            while(num > 0) {
-                if(num % 2 != 0) {
-                    count_1++;  // if odd (last bit 1) increment count of 1 
-                }
-                num = num >> 1; // right shift by 1
-            }
-
-            ans[i] = count_1;
+            ans[i] += ans[i/2] + (i % 2);
         }
 
         return ans;
