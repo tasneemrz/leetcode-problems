2e62f8600a0872facc39b0fc8f26ca73d3740cc4
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-09-17 20:55:17 +0530

    Time: 3 ms (96.55%), Space: 8.6 MB (54.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int maxLength = 0;
+        int l = 0, r = 0, maxFreq = 0;
+        int hash[26] = {0};
 
-        for(int i = 0; i < s.length(); i++) {
-            int charsChanged = 0;
-            int maxFreq = 0;
-            int hash[26] = {0};
+        while(r < s.length()) {
+            hash[s[r] - 'A']++;
+            maxFreq = max(maxFreq, hash[s[r] - 'A']);
+            int changes = (r - l + 1) - maxFreq;
 
-            for(int j = i; j < s.length(); j++) {
-                hash[s[j]-'A']++;
-                maxFreq = max(maxFreq, hash[s[j]-'A']);
-                charsChanged = (j-i+1) - maxFreq;
-                
-                if(charsChanged <= k) {
-                    maxLength = max(maxLength, j-i+1);
-                } else {
-                    break;
-                }
+            if(changes > k) {
+                hash[s[l] - 'A']--;
+                maxFreq = 0;
+                l++;
             }
+
+            if(changes <= k) {
+                maxLength = max(maxLength, r-l+1);
+            }
+
+            r++;
         }
 
         return maxLength;
