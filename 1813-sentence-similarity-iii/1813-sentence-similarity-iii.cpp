10e607406cf5045a932acc8ae06a53b75a2ad24e
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-10-06 23:18:44 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (7.37%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        istringstream iss1(sentence1);
+        deque<string> deque1((istream_iterator<string>(iss1)), istream_iterator<string>());
+        
+        istringstream iss2(sentence2);
+        deque<string> deque2((istream_iterator<string>(iss2)), istream_iterator<string>());
+        
+        while (!deque1.empty() && !deque2.empty() && deque1.front() == deque2.front()) {
+            deque1.pop_front();
+            deque2.pop_front();
+        }
+        
+        while (!deque1.empty() && !deque2.empty() && deque1.back() == deque2.back()) {
+            deque1.pop_back();
+            deque2.pop_back();
+        }
+        
+        return deque1.empty() || deque2.empty();
+        
+    }
+};
