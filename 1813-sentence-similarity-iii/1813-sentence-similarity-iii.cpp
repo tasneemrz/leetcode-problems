344fcdf2b6f3fa0015746b565170bc90920198e8
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-10-07 16:42:03 +0530

    Time: 4 ms (39.91%), Space: 9 MB (11.93%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     bool areSentencesSimilar(string sentence1, string sentence2) {
+        if(sentence1 == sentence2) return true; 
+
         istringstream iss1(sentence1);
-        deque<string> deque1((istream_iterator<string>(iss1)), istream_iterator<string>());
-        
+        vector<string> s1_words((istream_iterator<string>(iss1)), istream_iterator<string>());
+
         istringstream iss2(sentence2);
-        deque<string> deque2((istream_iterator<string>(iss2)), istream_iterator<string>());
-        
-        while (!deque1.empty() && !deque2.empty() && deque1.front() == deque2.front()) {
-            deque1.pop_front();
-            deque2.pop_front();
+        vector<string> s2_words((istream_iterator<string>(iss2)), istream_iterator<string>());
+
+        vector<string> commonWords;
+
+        // find longest common prefix
+        int start = 0;
+        while(start < s1_words.size() && start < s2_words.size()) {
+            if(s1_words[start] != s2_words[start]) {
+                break;
+            }
+            commonWords.push_back(s1_words[start]);
+            start++;
         }
-        
-        while (!deque1.empty() && !deque2.empty() && deque1.back() == deque2.back()) {
-            deque1.pop_back();
-            deque2.pop_back();
+        start--;
+
+        // find longest common suffix
+        int end1 = s1_words.size()-1, end2 = s2_words.size()-1;
+        while(end1 >= 0 && end2 >= 0 && start < end1 && start < end2) {
+            if(s1_words[end1] != s2_words[end2]) {
+                break;
+            }
+            commonWords.push_back(s1_words[end1]);
+            end1--;
+            end2--;
         }
-        
-        return deque1.empty() || deque2.empty();
-        
+
+        return commonWords.size() == s1_words.size() || commonWords.size() == s2_words.size();
     }
 };
