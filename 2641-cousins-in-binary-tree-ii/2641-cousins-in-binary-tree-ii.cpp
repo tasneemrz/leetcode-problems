ad491b4d089a4c7ba7d9f012ebcd578d207b7b66
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-10-23 21:16:57 +0530

    Time: 12 ms (98.00%), Space: 317.3 MB (26.50%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -15,36 +15,46 @@ public:
         if(root == nullptr) return root;
 
         queue<TreeNode*> q;
-        map<TreeNode*, int> siblingVal;
-
-        siblingVal[root] = 0;
         q.push(root);
+        int currLevelSum = root->val;
 
         while(!q.empty()) {
             int nodesRemaining = q.size();
-            int currLevelSum = 0;
-            vector<TreeNode*> currLevelNodes;
+            int nextLevelSum = 0;
 
             while(nodesRemaining--) {
                 TreeNode* currNode = q.front();
-                currLevelNodes.push_back(currNode);
-                currLevelSum += currNode->val;
 
-                if(currNode->left != nullptr) {
+                // current nodes values includes its siblings value as well (except if current = root node)
+                // subtracting its value from the total sum of current level gives cousins sum
+                currNode->val = currLevelSum - currNode->val;
+
+                // calculating current nodes children sums
+                int childSum = (currNode->left ? currNode->left->val : 0);
+                childSum += (currNode->right ? currNode->right->val : 0);
+
+                // next levels sum will have the sum of this nodes children
+                nextLevelSum += childSum;
+
+                // each childs value becomes sum of both child
+                // this is to ensure that when we subtract each child nodes value from its level's sum, 
+                // then its's sibling's value is also subtracted to get the cousins sum
+                if(currNode->left) {
+                    currNode->left->val = childSum; 
                     q.push(currNode->left);
-                    siblingVal[currNode->left] = (currNode->right) ? currNode->right->val : 0;
+                    
                 }
-                if(currNode->right != nullptr) {
+                if(currNode->right) {
+                    currNode->right->val = childSum; 
                     q.push(currNode->right);
-                    siblingVal[currNode->right] = (currNode->left) ? currNode->left->val : 0;
                 }
 
                 q.pop();
             }
 
-            for(auto node : currLevelNodes) {
-                node->val = currLevelSum - (node->val + siblingVal[node]); 
-            }
+            // now we move to the next level
+            // so the current level will become the next level whose sum we calculated above
+            currLevelSum = nextLevelSum;
         }
 
         return root;
