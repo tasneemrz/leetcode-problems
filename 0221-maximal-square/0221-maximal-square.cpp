45dc9f97c42320e69caf863d3613d7736fe26bfe
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-10-27 21:14:14 +0530

    Update 0221-maximal-square.cpp

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int rows = matrix.size();
+        int cols = matrix[0].size();
 
-        // cache to store the max length square that can be formed starting from each cell
-        // each cell will be taken as the top left corner of the square
-        vector<vector<int>> cache(m, vector<int>(n, -1));
+        // cache to store the max length square that can be formed for each cell
+        // each cell will be taken as the bottom right corner of the square
+        vector<vector<int>> cache(rows, vector<int>(cols, 0));
 
-        // base case for all the last columns of each row
-        for(int row = 0; row < m; row++) {
-            if(matrix[row][n-1] == '1')
-                cache[row][n-1] = 1;
-            else
-                cache[row][n-1] = 0;
-        }
-
-        // base case for all the last rows of each column
-        for(int col = 0; col < n; col++) {
-            if(matrix[m-1][col] == '1')
-                cache[m-1][col] = 1;
-            else
-                cache[m-1][col] = 0;
-        }
-
-        // iterate over all the other cells in matrix from bottom right
-        for(int i = m-2; i >= 0; i--) {
-            for(int j = n-2; j >= 0; j--) {
-                if(matrix[i][j] == '1') {
-                    cache[i][j] = min(cache[i+1][j], min(cache[i][j+1], cache[i+1][j+1])) + 1;
-                }
-                else {
-                    cache[i][j] = 0;
-                }
-            }
-        }
-
-        // iterate over cache to find the square with max side length 
+        // iterate over all the other cells in matrix from top to bottom
         int maxLength = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) { 
+                if(matrix[i][j] == '1') {
+                    // all cells in first row and column can only make a square of size 1
+                    if(i == 0 || j == 0) cache[i][j] = 1;
+                    else
+                        cache[i][j] = min({cache[i-1][j], cache[i-1][j-1], cache[i][j-1]}) + 1;
+                }
+                    
                 maxLength = max(maxLength, cache[i][j]);
             }
         }
