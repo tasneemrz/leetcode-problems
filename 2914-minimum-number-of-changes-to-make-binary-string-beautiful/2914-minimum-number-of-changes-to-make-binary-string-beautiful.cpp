6b3ace3acd20fe35dae6ea1b0e02fbaa9d8e162b
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-05 13:24:35 +0530

    Time: 4 ms (30.85%), Space: 12.1 MB (67.52%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
     int minChanges(string s) {
-        char currChar = s[0];
-        int consecutiveCount = 0;
         int minChangesReq = 0;
         
-        for(char c : s) {
-            if(c == currChar) {
-                consecutiveCount++;
-                continue;
-            } 
-            
-            if(consecutiveCount % 2 == 0) {
-                consecutiveCount = 1;
-            }
-            else {
-                consecutiveCount = 0;
+        for(int i = 0; i < s.length(); i+=2) {
+            if(s[i] != s[i+1]) 
                 minChangesReq++;
-            }
-            
-            currChar = c;
         }
         
         return minChangesReq;
