be7d642b3bb91760f6b7161b14ee6a7d912b73a4
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-06 13:18:08 +0530

    Time: 0 ms (100.00%), Space: 31.2 MB (86.44%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -5,29 +5,17 @@ public:
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n-i-1; j++) {
-                if(nums[j+1] < nums[j] && __builtin_popcount(nums[j+1]) == __builtin_popcount(nums[j]))
-                    swap(nums[j], nums[j+1]);
+                if(nums[j+1] >= nums[j]) 
+                    continue;
+                else {
+                    if(__builtin_popcount(nums[j+1]) == __builtin_popcount(nums[j]))
+                        swap(nums[j], nums[j+1]);
+                    else
+                        return false;
+                }
             }
         }
 
-        for(int i = 0; i < n-1; i++) {
-            if(nums[i] > nums[i+1]) 
-                return false;
-        }
-
         return true;
-    }
-
-private:
-    int setBits(int num) {
-        int countSetBits = 0;
-
-        while(num) {
-            if ((num & 1) != 0)
-                countSetBits++;
-            num >>= 1;
-        }
-
-        return countSetBits;
     }
 };
