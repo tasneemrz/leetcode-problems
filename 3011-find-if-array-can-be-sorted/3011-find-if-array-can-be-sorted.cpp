70ce4ffe3c16fc9146bca495469266a074c74aa0
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-07 12:45:00 +0530

    Time: 0 ms (100.00%), Space: 31.2 MB (86.38%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        int maxOfSegment = nums[0];
-        int minOfSegment = nums[0];
-        int numSetBits = __builtin_popcount(nums[0]);
-        int maxOfPrevSegment = INT_MIN;
-
-        for(int i = 1; i < nums.size(); i++) {
-            if(__builtin_popcount(nums[i]) == numSetBits) {
-                maxOfSegment = max(maxOfSegment, nums[i]);
-                minOfSegment = min(minOfSegment, nums[i]);
-            } 
-            else {
-                if(minOfSegment < maxOfPrevSegment) return false;
-                
-                maxOfPrevSegment = maxOfSegment;
-                maxOfSegment = nums[i];
-                minOfSegment = nums[i];
-                numSetBits = __builtin_popcount(nums[i]);
-            }      
+        int n = nums.size();
+        
+        // left to right pass
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] > nums[i+1]) {
+                if(__builtin_popcount(nums[i]) == __builtin_popcount(nums[i+1])) {
+                    swap(nums[i], nums[i+1]);
+                } 
+                else return false;
+            }     
         } 
         
-        if(minOfSegment < maxOfPrevSegment) return false;
+        // right to left pass
+        for(int i = n-1; i > 0; i--) {
+            if(nums[i] < nums[i-1]) {
+                if(__builtin_popcount(nums[i]) == __builtin_popcount(nums[i-1])) {
+                    swap(nums[i], nums[i-1]);
+                } 
+                else return false;
+            }     
+        } 
 
         return true;
     }
