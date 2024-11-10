c0feee55be66d4820c31f3b6cafec12635865a76
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-10 11:31:05 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (12.46%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,10 +1,41 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long result = x;
+        long long result = 0, bit;
+        --n;
         
-        while(--n) {
-            result = (result + 1) | x;
+        vector<int> binaryX(64, 0);
+        vector<int> binaryN(64, 0);
+        
+        long long longX = x;
+        long long longN = n;
+        
+        for(int i = 0; i < 64; i++) {
+            bit = (longX >> i) & 1;
+            binaryX[i] = bit;
+            
+            bit = (longN >> i) & 1;
+            binaryN[i] = bit;
+        }
+        
+        int posX = 0, posN = 0;
+        
+        while(posX < 63) {
+            // find a bit that is 0 in x that can be modified, 
+            // because we dont want to disturb the bits that are 1
+            while(binaryX[posX] != 0 && posX < 63) {
+                ++posX;
+            }
+            
+            binaryX[posX] = binaryN[posN];
+            ++posX;
+            ++posN;
+        }
+        
+        for(int i = 0; i < 64; ++i) {
+            if(binaryX[i] == 1) {
+                result += pow(2, i);
+            }
         }
         
         return result;
