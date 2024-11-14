8e1dcc5583444ffaa35d780e3da93244cfb1477a
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-14 16:35:30 +0530

    Time: 3 ms (95.22%), Space: 21.7 MB (43.99%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,27 +3,26 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
-        int ans = high+1;
         
-        while(low <= high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
             
-            if(totalHrsTaken(piles, mid) <= h) {
-                ans = min(ans, mid);
+            if (totalHrsTaken(piles, mid) <= h) {
                 high = mid - 1;
-            } else {
+            } 
+            else {
                 low = mid + 1;
             }
         }
         
-        return ans;
+        return low;
     }
     
 private:
     long long totalHrsTaken(vector<int>& piles, int hourly) {
         long long totalHrs = 0;
         
-        for(int i = 0; i < piles.size(); i++) {
+        for (int i = 0; i < piles.size(); i++) {
             totalHrs += (piles[i] + hourly - 1) / hourly;
         }
         
