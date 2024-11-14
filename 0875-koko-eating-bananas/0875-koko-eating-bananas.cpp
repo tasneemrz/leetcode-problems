8251263925b7ffe3f7d835f5df2bc9ad5b364450
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-14 16:39:46 +0530

    Time: 4 ms (90.03%), Space: 21.7 MB (43.99%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
+        int high = findMax(piles);
         
         while (low <= high) {
             int mid = (low + high) / 2;
@@ -19,6 +19,16 @@ public:
     }
     
 private:
+    int findMax(vector<int>& arr) {
+        int maxi = INT_MIN;
+        
+        for(int i = 0; i < arr.size(); i++) {
+            maxi = max(maxi, arr[i]);
+        }
+        
+        return maxi;
+    }
+    
     long long totalHrsTaken(vector<int>& piles, int hourly) {
         long long totalHrs = 0;
         
