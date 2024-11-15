52a062d56c7d9b98063900ccf9e27ba1263046ec
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-15 23:46:09 +0530

    Time: 1 ms (24.00%), Space: 69.4 MB (85.81%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -2,27 +2,37 @@ class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
         int n = arr.size();
-
-        int i = 0;
-        int j = n-1;
-        while (i < n-1) {
-            if (arr[i] > arr[i+1]) {
+        int left = 0;
+        int right = n - 1;
+        
+        // iterate the left pointer towards right hand side
+        while (left < n - 1) {
+            // if next element smaller then break
+            if (arr[left] > arr[left + 1]) {
                 break;
             }
-            i++;
+            left++;
         }
-        if (i == j) return 0;
-
-        while (j > 0) {
-            if (i > -1 && arr[i] > arr[j]) {
-                i--;
+        
+        // case : the array is already sorted
+        if (left == right) 
+            return 0;
+        
+        // iterate the right pointer from the end towards left hand side
+        while (right > 0) {
+            // last element of prefix is greater than first element of suffix
+            // then decrement left
+            if (left > -1 && arr[left] > arr[right]) {
+                left--;
             }
-            if (arr[j] < arr[j-1]) {
+            
+            // if next element greater then break
+            if (arr[right] < arr[right - 1]) {
                 break;
             }
-            j--;
+            right--;
         }
 
-        return j - i - 1;
+        return right - left - 1;
     }
 };
