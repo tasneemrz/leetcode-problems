05505916be2ffea9615980b13a5d22e0f10b4762
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-11-19 13:16:50 +0530

    Time: 117 ms (46.25%), Space: 94.9 MB (94.41%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -3,26 +3,26 @@ public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int i = 0, j = 0;
         long long maxSum = 0, sum = 0;
-        unordered_map<int, int> hash;
+        unordered_map<int, int> hashNumCount;
 
         while (i <= j && j < nums.size()) {
             if (j-i+1 <= k) {
                 sum += nums[j];
-                hash[nums[j]]++;
+                hashNumCount[nums[j]]++;
                 j++;
             }
             else {
-                if (hash.size() == k) 
+                if (hashNumCount.size() == k) 
                     maxSum = max(maxSum, sum);
                 
-                hash[nums[i]]--;
-                if(hash[nums[i]] == 0) hash.erase(nums[i]);
+                hashNumCount[nums[i]]--;
+                if(hashNumCount[nums[i]] == 0) hashNumCount.erase(nums[i]);
                 sum -= nums[i];
                 i++;
             }
         }
 
-        if(hash.size() == k) {
+        if(hashNumCount.size() == k) {
             maxSum = max(maxSum, sum);
         }
 
