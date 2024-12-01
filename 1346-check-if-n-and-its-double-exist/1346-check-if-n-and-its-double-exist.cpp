9b9d00bbb12479172ab918be8ccd08fd33f03e53
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-12-01 12:28:14 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (17.49%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,14 +1,35 @@
 class Solution {
+    int findDouble (vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
 public:
     bool checkIfExist(vector<int>& arr) {
-        unordered_set<int> seen;
+        sort(arr.begin(), arr.end());
         
-        for(int num : arr) {
-            if(seen.count(2 * num) || num % 2 == 0 && seen.count(num / 2)) {
-                return true;
-            }
+        for (int i = 0; i < arr.size(); i++) {
+            int target = arr[i] * 2;
+            int doubleIndex = findDouble(arr, target);
             
-            seen.insert(num);
+            if (doubleIndex >= 0 && doubleIndex != i)
+                return true;
         }
         
         return false;
