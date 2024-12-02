b107af9eaceef699a3f7eddcdd4a4a3238c93057
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-12-02 23:04:15 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (6.42%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        vector<string> words;
-        
         // extract the words from the input sentence
         istringstream iss(sentence);
         string word;
-        while(iss >> word) {
-            words.push_back(word);
-        }
         
-        // match each word with the given search word
-        for(int i = 0; i < words.size(); i++) {
-            string word = words[i];
+        int word_i = 1;
+        
+        // compare each word with the given search word
+        while(iss >> word) {
             int w_ptr = 0;
             int s_ptr = 0;
             
-            while(w_ptr < word.size() && s_ptr < searchWord.size()) {
+            while(w_ptr < word.length() && s_ptr < searchWord.length()) {
                 if(word[w_ptr] != searchWord[s_ptr]) {
                     break;
                 }
@@ -25,9 +21,11 @@ public:
             }
             
             // if match found return the index
-            if(s_ptr == searchWord.size()) {
-                return i + 1;
+            if(s_ptr == searchWord.length()) {
+                return word_i;
             }
+            
+            word_i++;
         }
         
         // match not found
