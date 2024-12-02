361b86f98dcbf64d1af56b9c129619db57ccefad
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-12-02 22:59:04 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (6.42%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
         vector<string> words;
+        
+        // extract the words from the input sentence
         istringstream iss(sentence);
         string word;
-        
         while(iss >> word) {
             words.push_back(word);
         }
         
+        // match each word with the given search word
         for(int i = 0; i < words.size(); i++) {
             string word = words[i];
             int w_ptr = 0;
@@ -22,11 +24,13 @@ public:
                 s_ptr++;
             }
             
+            // if match found return the index
             if(s_ptr == searchWord.size()) {
                 return i + 1;
             }
         }
         
+        // match not found
         return -1;
     }
 };
