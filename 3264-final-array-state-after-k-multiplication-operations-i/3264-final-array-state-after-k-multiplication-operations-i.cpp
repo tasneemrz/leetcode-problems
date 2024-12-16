15654ef40290ea47219fb376164e78a6c06446bb
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2024-12-16 22:41:02 +0530

    Time: 3 ms (30.95%), Space: 27.6 MB (29.75%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,23 +1,22 @@
 class Solution {
-    int minValueIndex(vector<int>& nums) {
-        int minIndex = 0;
-        int minValue = nums[0];
-        
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] < minValue) {
-                minValue = nums[i];
-                minIndex = i;
-            }
-        }
-        
-        return minIndex;
-    }
-    
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        while(k--) {
-            int minIndex = minValueIndex(nums);
-            nums[minIndex] *= multiplier; 
+        vector<pair<int, int>> heap;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            heap.push_back({nums[i], i});
+        }
+        
+        make_heap(heap.begin(), heap.end(), greater<>());
+        
+        while (k--) {
+            pop_heap(heap.begin(), heap.end(), greater<>());
+            auto [value, i] = heap.back();
+            heap.pop_back();
+            
+            nums[i] *= multiplier;
+            heap.push_back({nums[i], i});
+            push_heap(heap.begin(), heap.end(), greater<>());
         }
         
         return nums;
