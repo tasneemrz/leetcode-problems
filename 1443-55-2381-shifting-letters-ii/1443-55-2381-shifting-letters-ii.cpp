f45be6ec8572fca31c6ac00eb017c6d5f7ca1db6
Tasneem Rizwan <98607176+tasneemrz@users.noreply.github.com>
2025-01-05 23:30:27 +0530

    Time: 54 ms, Memory: 109.2 MB - LeetHub

---
+++ 1443-55-2381-shifting-letters-ii/1443-55-2381-shifting-letters-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        vector<int> diff(n + 1, 0);
+
+        for (auto shift : shifts) {
+            if (shift[2] == 1) {
+                diff[shift[0]]++;
+                diff[shift[1] + 1]--;
+            } else {
+                diff[shift[0]]--;
+                diff[shift[1] + 1]++;
+            }
+        }
+
+        for (int i = 1; i <= n; ++i)
+            diff[i] += diff[i - 1];
+
+        string res;
+        for (int i = 0; i < n; ++i) {
+            int count = diff[i] % 26;
+            if (count < 0)
+                count = 26 + count;
+
+            char curr = char((s[i] - 'a' + count) % 26 + 97);
+            res.push_back(curr);
+        }
+
+        return res;
+    }
+};
